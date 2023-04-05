dc863fac1cd8eb83d99c9ccdd5a940bb430fdb19
ocetot <perisjara@gmail.com>
2023-04-05 17:57:08 -0400

    ejer 3

--- js/app.js
+++ js/app.js
@@ -5,3 +5,7 @@ let b = 120; // let arranca... desde ES6
 let myDom = document;
 
 console.log(myDom);
+
+
+let nombre = prompt("Por favor ingresa tu nombre:");
+alert("Hola " + nombre + ", bienvenido!");
